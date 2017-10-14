85e1a0669c481083d50e21ba8e508b9b8335a99a
Richard Hill III <rhillz718@gmail.com>
2017-10-13 23:19:29 -0400

    initial commit and added redux

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import {Header} from './src/components/common';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerTitle='Tech-Stack'/>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+
